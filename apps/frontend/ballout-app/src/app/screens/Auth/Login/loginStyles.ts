eed5dd2ee7651edc018ecba96b4eaa05fab93dd9
vishnu <vishnusethuraman@gmail.com>
2025-01-17 23:55:53 +0530

    refactored auth to contain login and signup screens, changed relative to absolute imports, added login, sigup screens, need to integrate redux

---
+++ apps/frontend/ballout-app/src/app/screens/Auth/Login/loginStyles.ts
@@ -0,0 +1,22 @@
+import { Platform, StyleSheet } from 'react-native';
+
+export const LoginStyles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap:20,
+  },
+  loginHeader: {
+    color: 'white',
+    fontSize:25,
+    marginTop:40,
+    marginBottom:20,
+    fontFamily: 'Orbitron',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+    fontWeight: 800,
+  }
+
+});
