283438591e8e06690c89bf2211cfc413931bbdea
vishnu <vishnusethuraman@gmail.com>
2025-01-18 00:29:34 +0530

    working on login, currently logs email,pass in console on press of button, need to integrate to loginhandler and validations

--- apps/frontend/ballout-app/src/app/components/CTA/index.tsx
+++ apps/frontend/ballout-app/src/app/components/CTA/index.tsx
@@ -1,33 +1,37 @@
 import { Button, Image, Text, TouchableOpacity, View } from 'react-native';
-import { CTAStyles } from './CTAStyles';
-import { useFontInComponent } from '../../hooks/useFontInComponent';
+import { CTAStyles } from '@ballout-app/src/app/components/CTA/CTAStyles';
+import { useFontInComponent } from '@ballout-app/src/app/hooks/useFontInComponent';
 import { LinearGradient } from 'expo-linear-gradient';
 
 interface CTAProps {
-  text: string,
-  arrow?: boolean,
-  uppercase?: boolean
+  text: string;
+  onPress: () => void;
+  arrow?: boolean;
+  uppercase?: boolean;
 }
 
 export const CTA = (props: CTAProps) => {
-  const fontLoaded = useFontInComponent(['Montserrat'])
+  const fontLoaded = useFontInComponent(['Montserrat']);
   if (!fontLoaded) return;
 
   return (
-      <TouchableOpacity>
-        <LinearGradient
-          start={{x: 0, y: 0}} end={{x: 1, y: 0}}
-          colors={['#DA6809', '#FF9F53']}
-          style={CTAStyles.gradient}
-        >
-          <Text style={CTAStyles.btnText}>{
-            props.uppercase ? props.text.toUpperCase() : props.text
-          }</Text>
-          {
-            props.arrow &&
-            <Image source={require('../../../../assets/arrow_right.png')} style={CTAStyles.arrow} />
-          }
-        </LinearGradient>
-      </TouchableOpacity>
-  )
-}
+    <TouchableOpacity onPress={props.onPress}>
+      <LinearGradient
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+        colors={['#DA6809', '#FF9F53']}
+        style={CTAStyles.gradient}
+      >
+        <Text style={CTAStyles.btnText}>
+          {props.uppercase ? props.text.toUpperCase() : props.text}
+        </Text>
+        {props.arrow && (
+          <Image
+            source={require('../../../../assets/arrow_right.png')}
+            style={CTAStyles.arrow}
+          />
+        )}
+      </LinearGradient>
+    </TouchableOpacity>
+  );
+};
