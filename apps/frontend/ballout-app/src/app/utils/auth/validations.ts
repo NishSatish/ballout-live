66daeba31325c9c73a05cc24d9ad7abe7e9209a1
vishnu <vishnusethuraman@gmail.com>
2025-01-18 15:55:36 +0530

    feat: added validation utils for email and password

---
+++ apps/frontend/ballout-app/src/app/utils/auth/validations.ts
@@ -0,0 +1,23 @@
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+export const isValidEmail = (email: string): boolean => {
+  return emailRegex.test(email);
+};
+
+export const isValidPassword = (password: string): boolean => {
+  if (!password) return false; // Ensure password is not empty
+
+  const minLength = 8;
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasNumber = /\d/.test(password);
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+  return (
+    password.length >= minLength &&
+    hasUpperCase &&
+    hasLowerCase &&
+    hasNumber &&
+    hasSpecialChar
+  );
+};
